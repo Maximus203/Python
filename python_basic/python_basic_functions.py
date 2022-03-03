46a9b1a87372cbf9377791108f8ac2ccb61bef9c
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-03-03 19:16:40 +0000

    Hello world

---
+++ python_basic/python_basic_functions.py
@@ -0,0 +1,11 @@
+help(print) # Use to display the documentation of modules
+
+a = 1
+id(print) #id function return the identity of an object
+
+type(a) #return the type of an object
+
+del(a) #deletes an object
+print(a)
+
+len("hello") #returns the length of an object
